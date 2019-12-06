627bf590820963d5f81a920e8e06f7943a9dfcb9
shivanisaini347 <shivanisaini347@gmail.com>
2019-12-06 21:24:19 +0530

    Dynamic Content: Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import Header from './Header.js';
 import './App.css';
 class App extends Component{
   render() {
+    let subscribers = [
+      {
+        id: 1,
+        name: "Shilpa Bhat",
+        phone: "8888888888"
+      },
+      {
+        id: 2,
+        name: "Srishti Gupta",
+        phone: "9999999999"
+      }
+    ];
     return (
       <div>
         <Header />
