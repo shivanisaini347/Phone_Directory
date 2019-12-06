5adb98483947ba481972bbb93a00a296f726bf4b
shivanisaini347 <shivanisaini347@gmail.com>
2019-12-06 22:02:25 +0530

    State: Added state in ‘AddSubscriber’ and ‘App’ components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,13 @@ import React, {Component} from 'react';
 import Header from './Header.js';
 import './App.css';
 class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+    subscribersListToShow: []
+    }
+  }
   render() {
-    let subscribers = [
-      {
-        id: 1,
-        name: "Shilpa Bhat",
-        phone: "8888888888"
-      },
-      {
-        id: 2,
-        name: "Srishti Gupta",
-        phone: "9999999999"
-      }
-    ];
     return (
       <div>
          <Header heading="Phone Directory" />
@@ -27,7 +21,7 @@ class App extends Component{
           </div>
 
           {
-            subscribers.map(sub => {
+            this.state.subscribersListToShow.map(sub => {
               return <div key={sub.id} className="grid-container">
                 <span className="grid-item">{sub.name}</span>
                 <span className="grid-item">{sub.phone}</span>
--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import AddSusbscriber from './AddSubscriber';
 import './common/common.css';
 
-ReactDOM.render(<AddSusbscriber  />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-document.getElementById('root'));
+
 serviceWorker.unregister();
